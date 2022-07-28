f4692c758a52a2d3aa067aed7a74db1484a69e59
Hema.v <hema.v@stackroute.in>
2022-07-29 00:16:31 +0530

    Initial commit

---
+++ wat-a-pizza-solution.js
@@ -0,0 +1,94 @@
+let menu = [
+    {
+        'category': 'Beverages',
+        'name': 'Coke',
+        'price': 1.5
+    },
+    {
+        'category': 'Starters',
+        'name': 'Garlic Bread',
+        'price': 2.8
+    },
+    {
+        'category': 'Starters',
+        'name': 'Mozzarella Sticks',
+        'price': 5.5
+    },
+    {
+        'category': 'Main Meal',
+        'name': 'Medium Size Margherita Pizza',
+        'price': 11
+    },
+    {
+        'category': 'Beverages',
+        'name': 'Iced Tea',
+        'price': 1.25
+    },
+    {
+        'category': 'Starters',
+        'name': 'Greek Wedge Salad',
+        'price': 4.5
+    },
+    {
+        'category': 'Main Meal',
+        'name': 'Veg Family Meal',
+        'price': 13.25
+    },
+    {
+        'category': 'Main Meal',
+        'name': 'Large Size Vegan Pepperoni Pizza',
+        'price': 14.5
+    },
+]
+const order = {
+    'items': [
+        {
+            'name': 'Mozzarella Sticks',
+            'price': 5.5,
+            'quantity': 2
+        },
+        {
+            'name': 'Garlic Bread',
+            'price': 2.8,
+            'quantity': 2
+        },
+        {
+            'name': 'Coke',
+            'price': 1.5,
+            'quantity': 3
+        },
+        {
+            'name': 'Medium Size Margherita Pizza',
+            'price': 11,
+            'quantity': 2
+        },
+        {
+            'name': 'Iced Tea',
+            'price': 1.25,
+            'quantity': 1
+        },
+        {
+            'name': 'Veg Family Meal',
+            'price': 13.25,
+            'quantity': 2
+        },
+    ]
+};
+
+//Write function to filter menu items by Category and sort them alphabetically
+
+//Write function to calculate the amount of each ordered item
+
+//Write function to calculate the subtotal amount for the main meal ordered
+
+//Write function to find the totalAmount of the order based on the calculated mainMealAmount value
+
+//Write function to calculate the final amount after discount
+
+//Write function to display the total amount 
+
+//Write function to display the final amount
+
+//Invoke  functions 
+
+
